d99430c182697e7437cc340f8df183558211cdda
Federico Matteo <52129760+FedeMatt@users.noreply.github.com>
2019-09-13 16:30:26 +0300

    Update Bank Project.R

--- Bank Project.R
+++ Bank Project.R
@@ -1,8 +1,8 @@
 # Find the best strategies to improve for the next marketing campaign. 
 # How can the financial institution have a greater effectiveness for 
 # future marketing campaigns? In order to answer this, we have to analyze
-# the last marketing campaign the bank performed and identify the patterns 
-# that will help us find 
+# the last marketing campaign the bank performed and identify from the data
+# the costumers profiles
 
 
 # load packages and set seed
